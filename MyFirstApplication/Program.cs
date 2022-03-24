06ff6ca58f709b2e919adb432c844128d8733839
omunoz <omarmunoz121@yahoo.com>
2022-03-24 18:41:52 -0500

    CAC-103-C#Overview

---
+++ MyFirstApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using MyFirstApplication;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// The below statement calls a local function
+// single line comment
+HouseExample();
+
+/*
+ Multi-line comment
+The below statement is a function. Local functions 
+are inside of members and are private by default. This means
+They are specific to that member and can only be called 
+inside that member. 
+ */ 
+void HouseExample()
+{
+    House myHouse = new House();
+    myHouse.DoorOpenClose();
+}
+
