849fbb22c5bb5052f6f1b977517f1f27ad41103e
omunoz <omarmunoz121@yahoo.com>
2022-04-23 23:35:28 -0500

    Exercise18

--- MyFirstTestProject/Lesson10Test.cs
+++ MyFirstTestProject/Lesson10Test.cs
@@ -22,8 +22,31 @@ namespace MyFirstTestProject
             Assert.AreEqual(firstName, myLesson10.FirstName);
             Assert.AreEqual(lastName, myLesson10.LastName);
             Assert.AreEqual(zipCode, myLesson10.Zipcode);
-            
 
         }
+            /*
+             Write a test method inside the Lesson10Test.cs file. 
+            Ensure you have the [TestMethod] above this method. This method will be called IdVerify.
+            We will be utilizing the arrange and assert portions of the AAA pattern.
+            Create a variable of int called id. Provide a value for this variable. 
+            Create an object of Lesson 10 using the default constructor. 
+            Using the object created, assign the id variable to the Id property
+            Using an Assert, we are going to check to see if theid property was assigned properly,
+            or if its still using the default value of 1000. Pick through the Assert methods to determine which would fit. 
+            There are more than 1 possible method to test this, including the one used in the lecture.
+            Run the test several times to ensure you get both a passing test and a failure. 
+            Ensure you provide a passing version in your checked in code.You only need to provide 1 Assert + method
+             */
+            [TestMethod]
+            public void IdVerify()
+            {
+                int id = 10;
+                Lesson10 myLesson10 = new Lesson10();
+                myLesson10.Id = id;
+
+                Assert.AreEqual(id, myLesson10.Id);
+
+            }
+        
     }
 }
